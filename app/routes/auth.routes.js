4c7697f8ef50871f0701f6c555543b9e4f8b2099
mayank.kushwah <mayank.kushwah@eagleview.com>
2024-04-30 14:13:34 +0530

    Refactor authentication and user routes, add rate limiting, helmet and compression middleware

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,14 +2,6 @@ import verifySignUp from '../middleware/verifySignUp.js';
 import {signup, signin} from '../controllers/auth.controller.js';
 
 const authRoutes = (app) => {
-  app.use((req, res, next) => {
-    res.header(
-      'Access-Control-Allow-Headers',
-      'x-access-token, Origin, Content-Type, Accept'
-    );
-    next();
-  });
-
   app.post(
     '/api/auth/signup',
     [
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,14 +3,6 @@ import { getUserDetails, updateUsers, adminBoard } from '../controllers/userCont
 import validate from '../Validations/userValidation.js';
 
 export default function userRoutes(app) {
-  app.use((req, res, next) => {
-    res.header(
-      'Access-Control-Allow-Headers',
-      'x-access-token, Origin, Content-Type, Accept'
-    );
-    next();
-  });
-
   app.get( '/api/user', [authJwt.verifyToken], getUserDetails);
   app.put( '/api/user', [authJwt.verifyToken, validate.userUpdateValidation], updateUsers);
   app.get('/api/admin',[authJwt.verifyToken, authJwt.isAdmin],adminBoard);
